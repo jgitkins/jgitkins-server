becb2f9d317c52f95085edca034e50b3a17896db
hrllk <roovcloudy@gmail.com>
2026-02-23 21:50:32 +0900

    Update Tmp

--- src/main/java/io/jgitkins/server/infrastructure/config/git/GitSmartHttpEvent.java
+++ src/main/java/io/jgitkins/server/infrastructure/config/git/GitSmartHttpEvent.java
@@ -1,3 +1,5 @@
 package io.jgitkins.server.infrastructure.config.git;
 
-public record GitSmartHttpEvent(String ownerName, String repositoryName) {}
+public record GitSmartHttpEvent(String ownerName, String repositoryName) {
+
+}
